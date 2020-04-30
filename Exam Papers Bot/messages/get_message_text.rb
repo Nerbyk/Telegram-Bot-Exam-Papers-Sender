02fd6425f398b330d5bcca00c76b703499fed521
Nerbyk <danil.maximov2000@gmail.com>
2020-04-30 08:20:10 +0200

    reply text via yaml

---
+++ Exam Papers Bot/messages/get_message_text.rb
@@ -0,0 +1,25 @@
+require 'yaml'
+
+
+class GetMessageText
+  attr_reader :client, :replies_list
+  def initialize(client: 'user')
+    @client         = client
+    @replies_list   = YAML.load(File.read(client + '_messages.yml'))
+  end
+
+  def reply(case_text)
+    text = replies_list[case_text]
+  end
+
+end
+
+text = GetMessageText.new()
+p text.reply('greeting_first_time_user')
+
+
+
+# replies = YAML.load(
+#   File.read("./user_messages.yml"))
+#   text = replies["get_user_info_link"]
+#   p text
