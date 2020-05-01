8dc13af1717ef95e8eca8cdbf0cb1b29a834a85e
Nerbyk <danil.maximov2000@gmail.com>
2020-05-01 07:32:18 +0200

    database class improvement for user step control

--- Exam Papers Bot/app/Database/database.rb
+++ Exam Papers Bot/app/Database/database.rb
@@ -4,12 +4,12 @@ require 'sqlite3'
 require 'sequel'
 
 class Database
-  attr_reader :db, :id, :dataset, :table
+  attr_reader :db, :id, :dataset, :table, :status
   def initialize(id: nil, status: nil)
-    @id         = id
-    @status     = status
+    @id         = id.to_s
+    @status     = status
     @table      = :user_details
-    @db         = Sequel.sqlite('user_details.db')
+    @db         = Sequel.sqlite('./Database/user_details.db')
     @dataset    = create
   end
 
@@ -24,14 +24,23 @@ class Database
       String :status
     end
     dataset = db[table]
-  end
+  end
+
+  def registrate
+    p verificate
+    dataset.insert(user_id: id, status: status) if !verificate
+   puts("already registred") if verificate
+  end
 
   def verificate
     dataset.each do |row|
-      if row.values[1] == id && row.values[6] == 'in progress'
-        return row.values[6]
-      end
+      p row
+      return row.values[6] if row.values[1] == id && row.values[6] == 'in progress'
       return row.values[6] if row.values[1] == id && row.values[6] == 'accepted'
+      return row.values[6] if row.values[1] == id && row.values[6] == 'registered'
+      return row.values[6] if row.values[1] == id && row.values[6] == 'name'
+      return row.values[6] if row.values[1] == id && row.values[6] == 'link'
+      return true if row.values[1] == id && row.values[6] == nil
     end
     false
   end
@@ -39,4 +48,9 @@ class Database
   def make_record(user_data)
     dataset.insert(user_id: user_data[0], name: user_data[1], link: user_data[2], subjects: user_data[3], image: user_data[4], status: user_data[5])
   end
+
+  def update_status
+    dataset.filter(user_id: id).update(:status => status)
+  end
+
 end
