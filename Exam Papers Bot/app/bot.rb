2b7afd9c88f0e4432619dccfe6498c86ac9119f4
Nerbyk <danil.maximov2000@gmail.com>
2020-05-01 11:48:52 +0200

    exception handling

--- Exam Papers Bot/app/bot.rb
+++ Exam Papers Bot/app/bot.rb
@@ -6,15 +6,22 @@ require './messages/responder.rb'
 require './messages/button.rb'
 Dotenv.load('./.env')
 
+
+
 Telegram::Bot::Client.run(ENV['TOKEN']) do |bot|
   bot.listen do |message|
-    case message
-    when Telegram::Bot::Types::Message
-      message_responder = MessageResponder.new
-      message_responder.call(bot: bot, message: message)
-    when Telegram::Bot::Types::CallbackQuery
-      message_button = MessageButton.new
-      message_button.call(bot: bot, message: message)
+     begin
+      case message
+      when Telegram::Bot::Types::CallbackQuery
+        message_button = MessageButton.new
+        message_button.call(bot: bot, message: message, user_input: nil)
+      else
+        message_responder = MessageResponder.new
+        message_responder.call(bot: bot, message: message, user_input: message.text)
+      end
+    rescue Exception => e
+      bot.api.send_message(chat_id: message.from.id, text: "Пожалуйста введите данные требуемого формата.")
+      p e
     end
   end
 end
