b5e15864ed952faf9295a1b4222c75d698a66fe6
Nerbyk <danil.maximov2000@gmail.com>
2020-05-01 11:48:36 +0200

    subjects validation methods

--- Exam Papers Bot/app/messages/actions/check_input.rb
+++ Exam Papers Bot/app/messages/actions/check_input.rb
@@ -1,16 +1,27 @@
 class Check
+  MAX_SUBJECTS = 6
     def name(name)
       return false if name.split(' ').length > 2 || name.split.length == 1
       true
     end
+
     def membership(link)
       if link == 'false'
-        puts("FALSE")
         return false
       else
-        puts("TRUE")
         return true
-      end 
+      end
+    end
+
+    def each_subject(input)
+      available_list = %w[Биология География Химия Информатика Английский История Физика Закончить\ Ввод]
+      available_list.include?(input) ? false : true
+    end
+
+    def all_subjects(subjects)
+      p subjects
+      false if subjects.length > MAX_SUBJECTS || subjects.length > subjects.uniq.length
+      true 
     end
 
 end
