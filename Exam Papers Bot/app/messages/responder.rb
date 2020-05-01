3cee0e1957a5c6b960437164c2317620751eafab
Nerbyk <danil.maximov2000@gmail.com>
2020-05-01 11:46:38 +0200

    Send photo class was added

--- Exam Papers Bot/app/messages/responder.rb
+++ Exam Papers Bot/app/messages/responder.rb
@@ -10,11 +10,12 @@ require 'dotenv'
 Dotenv.load('./.env') # to get admin user_id
 
 class MessageResponder
-  attr_reader :bot, :message, :my_text, :client_id, :verification, :user_input
+  attr_reader :bot, :message, :user_input, :my_text, :client_id, :verification, :import_info
   include RequestMethods
-  def call(bot:, message:)
+  def call(bot:, message:, user_input:)
     @bot          = bot
     @message      = message
+    @user_input   = user_input
     @my_text      = GetMessageText.new
     @client_id    = message.from.id
     @verification = Verification.new.call(client_id: client_id)
@@ -23,18 +24,22 @@ class MessageResponder
 
   def respond(client_id)
     #  if client_id != ENV['ADMIN_ID'] # if user not admin
-    if message.text == '/start'
+    if user_input == '/start'
       answer_with_greeting_message unless verification
       answer_with_in_progress_notification if verification == 'in progress'
       answer_with_accepted_notification if verification == 'accepted'
     end
-
-    if verification == 'registered'
+    case verification
+    when 'registered'
       get_name
-    elsif verification == 'name'
+    when 'name'
       get_link
-    elsif verification == 'link'
-      puts 'link'
+    when 'link'
+      get_subjects
+    when 'subjects'
+      get_photo
+    when 'photo'
+      show_request
     end
     #  end
   end
@@ -55,10 +60,20 @@ class MessageResponder
   def answer_with_message(text, reply_markup = nil)
     SendMessage.new.call(bot: bot, chat: message.chat, text: text, reply_markup: reply_markup)
   end
+
+  def send_photo(caption, photo, reply_markup)
+    SendPhoto.new.call(bot: bot, chat: message.chat,photo: photo, caption: caption, reply_markup: reply_markup)
+  end
 end
 
 class SendMessage
   def call(bot:, chat:, text:, reply_markup:)
     bot.api.send_message(chat_id: chat.id, text: text, reply_markup: reply_markup)
+  end
+end
+
+class SendPhoto
+  def call(bot:, chat:, photo:, caption:, reply_markup:)
+    bot.api.send_photo(chat_id: chat.id, photo: photo, caption: caption, reply_markup: reply_markup)
   end
 end
