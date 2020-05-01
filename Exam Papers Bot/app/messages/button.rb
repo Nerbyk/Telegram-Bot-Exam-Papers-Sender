054dfb01a704e5f2f71e749ffe58cf3dd51590e9
Nerbyk <danil.maximov2000@gmail.com>
2020-05-01 07:30:10 +0200

    Start button functionality

--- Exam Papers Bot/app/messages/button.rb
+++ Exam Papers Bot/app/messages/button.rb
@@ -2,22 +2,30 @@
 
 require './messages/actions/get_message_text.rb'
 require './messages/request/get_user_info.rb'
+require './Database/database.rb'
+require './messages/responder.rb'
+
 class MessageButton < MessageResponder
   attr_reader :bot, :message, :my_text
   def call(bot:, message:)
     super
   end
 
-  def respond(_client_id)
+  def respond(client_id)
     if message.data == 'Start'
+      db = Database.new(id:client_id, status: 'registered').registrate
       edit_buttoned_text(my_text.reply('greeting_first_time_user'))
-      get_user_info = GetUserInfo.new
-      get_user_info.call(bot: bot, message: message)
+      puts("Start pressed")
+      answer_with_message(my_text.reply('get_user_info_name'))
     end
   end
 
   def edit_buttoned_text(text)
     EditMessage.new.call(bot: bot, chat: message.message.chat, message_id: message, text: text)
+  end
+
+  def answer_with_message(text, reply_markup = nil)
+    SendMessage.new.call(bot: bot, chat: message.message.chat, text: text, reply_markup: reply_markup)
   end
 end
 
