f50cc44eda6f83b5db80e42bc9dab89ba06b01ae
Nerbyk <danil.maximov2000@gmail.com>
2020-05-02 10:20:01 +0200

    Full functionality for admins

---
+++ Exam Papers Bot/app/messages/admin_buttons.rb
@@ -0,0 +1,36 @@
+require './messages/admin_responder.rb'
+require './messages/files_to_send/send_files.rb'
+
+
+class AdminButton < AdminResponder
+  attr_reader :bot, :message, :user_input, :my_text, :client_id
+  def call(bot:, message:, user_input:)
+    super
+  end
+  def respond(client_id)
+    case message.data
+    when 'Accept'
+      request = Database.new.admin_get_request
+      subjects = request[:subjects]
+      user_id = request[:user_id].to_i
+      answer_to_client(user_id, my_text.reply('inpect_accept_message_to_user'))
+      SendFiles.new(bot, user_id, subjects)
+      Database.new(id: user_id.to_s, status: Status::ACCEPTED).update_data
+      inspect_requests
+    when 'Deny'
+      request = Database.new.admin_get_request
+      user_id = request[:user_id].to_i
+      answer_to_client(user_id, my_text.reply('inspect_deny_message_to_user'))
+      Database.new(id: user_id.to_s).delete_user_progress
+      inspect_requests
+    when 'Ban'
+      request = Database.new.admin_get_request
+      user_id = request[:user_id].to_i
+      answer_to_client(user_id, my_text.reply('insepct_ban_message_to_user'))
+      Database.new(id: user_id.to_s, status: Status::BAN).update_data
+    when 'Menu'
+      answer_menu
+    end
+  end
+
+end
---
+++ Exam Papers Bot/app/messages/admin_responder.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require './messages/actions/get_message_text.rb'
+require './messages/actions/inline_markup.rb'
+require './messages/responder.rb'
+require './Database/database.rb'
+require './messages/status_constants.rb'
+
+class AdminResponder < MessageResponder
+  attr_reader :bot, :message, :user_input, :my_text, :client_id
+  def call(bot:, message:, user_input:)
+    @bot          = bot
+    @message      = message
+    @user_input   = user_input
+    @client_id    = message.from.id
+    @my_text      = GetMessageText.new(client: 'admin')
+    respond(client_id)
+  end
+
+  def respond(client_id)
+      case user_input
+      when '/start'
+        answer_menu
+      when '/inspect'
+        inspect_requests
+      when '/amount'
+        get_amount_of_uninspected
+      end
+  end
+
+  def answer_menu
+    answer_with_message(my_text.reply('greeting_menu'))
+  end
+
+  def inspect_requests
+    markup = MakeInlineMarkup.new(['Одобрить', 'Accept'], ['Отказать', 'Deny'], ['Забанить','Ban'], ['Вернуться в Главное меню','Menu']).get_markup
+    request = Database.new.admin_get_request
+    user_name = ''
+    request[:user_name] == nil ? user_name = 'N/A' : user_name = '@' + request[:user_name]
+    if !request
+      answer_with_message("Нет новых заявок")
+    else
+      if request[:status] == Status::INSPECTING && request[:status] != []
+        answer_with_message(my_text.reply('not_ended_inspection'))
+        send_photo("Имя Фамили: #{request[:name]}\nПредметы:#{request[:subjects]}\nTelegram:#{user_name}\nСсылка на ВК: #{request[:link]}", request[:image], markup)
+      elsif request[:status] == Status::IN_PROGRESS && request[:status] != []
+        send_photo("Имя Фамили: #{request[:name]}\nПредметы:#{request[:subjects]}\nTelegram:#{user_name}\nСсылка на ВК: #{request[:link]}", request[:image], markup)
+      end
+    end
+  end
+
+  def get_amount_of_uninspected
+    amount = Database.new.get_number
+    answer_with_message(my_text.reply("amount_message")+"#{amount}")
+    answer_menu
+  end
+
+  def answer_to_client(chat_id, text, reply_markup = nil)
+    SendMessage.new.call(bot: bot, chat: chat_id, text: text, reply_markup: reply_markup)
+  end
+end
